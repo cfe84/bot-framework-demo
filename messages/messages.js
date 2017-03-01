a1a61a2702dd03cff9a261c676bb69e4230df422
Charles Feval <github@crap.feval.fr>
2017-03-01 13:09:02 -0800

    It's broken

--- messages/messages.js
+++ messages/messages.js
@@ -1,11 +1,14 @@
 var pick = (array) => array[Math.floor(Math.random() * array.length)];
 
 var weTriedIt = (something) =>
-  [ `We tried ${something} at EA, it doesn't work`, `${something} are useless`, `I don't believe in ${something}` ]
+  [ `We tried ${something} at EA, it doesn't work`, `${something} are useless`, `I don't believe in ${something}`, `${something} sucks`]
 
 var quotes = {
-  generalitiesAboutEa: ["EA is awesome", "At EA everything was cool", "We actually understood what we were doing at EA"],
-  unitTests: weTriedIt("unit tests").concat(["Developers shouldn't write unit tests"])
+  generalitiesAboutEa: ["EA is awesome", "At EA everything was cool", "We actually understood what we were doing at EA", "This place is beneath me"],
+  unitTests: weTriedIt("unit tests").concat(["Developers shouldn't write unit tests"]),
+  AWS: weTriedIt("AWS"),
+  tried: ["We tried it at EA", "I know how it works"],
+  notWorking: ["It's not my fault", "I didn't break it", "It works on my machine", "It's not me it's you"]
 };
 
 var process = function(message){
@@ -14,16 +17,30 @@ var process = function(message){
     text: ""
   };
 
+  setResponse = (array) =>
+  {
+    response.respond = true;
+    response.text = pick(array);
+  }
+
   switch (true)
   {
     case /\bEA\b/.test(message.text):
-    case /\belectronic arts\b/i.test(message.text):
-      response.respond = true;
-      response.text = pick(quotes.generalitiesAboutEa);
+    case /electronic art/i.test(message.text):
+      setResponse(quotes.generalitiesAboutEa);
       break;
-    case /\bunit test\b/i.test(message.text):
-      response.respond = true;
-      response.text = pick();
+    case /unit test/i.test(message.text):
+      setResponse(quotes.unitTests);
+      break;
+    case /not working/.test(message.text):
+    case /doesn\'t work/.test(message.text):
+    case /\bfail/.test(message.text):
+    case /\bbroken\b/.test(message.text):
+      setResponse(quotes.notWorking);
+      break;
+    case /\btry\b/i.test(message.text):
+    case /\btried\b/i.test(message.text):
+      setResponse(quotes.tried);
       break;
   }
 
--- messages/test/testMessages.js
+++ messages/test/testMessages.js
@@ -16,11 +16,31 @@ testMessage = (text, matchResponse = null) => {
 
 describe("Message processing", () => {
 
-    it("Shouldn't react to triviality", () => {
-        testMessage("Hello, how are you?");
-    });
+  it("Shouldn't react to triviality", () => {
+      ["Hello, how are you?"].forEach((message) => testMessage(message));
+  });
 
-    it("Should react to EA", () => {
-        testMessage("At EA stuff is screwed up", processor.quotes.generalitiesAboutEa);
-    });
+  it("Should react to EA", () => {
+    ["Do you like EA?", "At EA stuff is screwed up", "EA"].forEach((message) =>
+      testMessage(message, processor.quotes.generalitiesAboutEa));
+  });
+
+  it("Should not overreact to EA", () => {
+    ["Each time I hear that", "You're being mean", "It's a BEACH"].forEach((message) => testMessage(message))
+  });
+
+  it("Should react to Unit tests", () => {
+    ["I don't like unit tests", "Did you write unit tests?", "I'm on this Unit Test"].forEach((message) =>
+      testMessage(message, processor.quotes.unitTests));
+  });
+
+  it("Should be apologetic", () => {
+    ["The build is broken", "It doesn't work", "It's not working", "The build failed.", "Some tests are failing"].forEach((message) =>
+      testMessage(message, processor.quotes.notWorking));
+  });
+
+  it("Should be say it tried", () => {
+    ["I've tried everything", "Did you try this thing"].forEach((message) =>
+      testMessage(message, processor.quotes.tried));
+  });
 });
